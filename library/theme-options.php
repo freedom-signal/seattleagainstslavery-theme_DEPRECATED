cfa3054a0e690b48f335dc6ebc8bcbd52cc78308
Marina Berti <marina@codesqwerl.com>
2018-03-10 16:45:59 -0800

    feat(theme): add customization options

---
+++ library/theme-options.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Add postMessage support
+ */
+function customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport = 'postMessage';
+
+	$wp_customize->add_setting( 'sas_logo' );
+	$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'sas_logo', array(
+		'label'    => __( 'Logo', 'foundationpress' ),
+		'section'  => 'title_tagline',
+		'settings' => 'sas_logo',
+	) ) );
+
+	$wp_customize->add_section( 'contact_social', array(
+		'priority' => 30,
+		'title'    => __( 'Contact & Social', 'foundationpress' )
+	) );
+	$wp_customize->add_setting( 'sas_phone' );
+	$wp_customize->add_control( 'sas_phone', array(
+		'label'   => __( 'Phone Number', 'foundationpress' ),
+		'section' => 'contact_social',
+		'type'    => 'text'
+	) );
+	$wp_customize->add_setting( 'sas_email' );
+	$wp_customize->add_control( 'sas_email', array(
+		'label'   => __( 'Email Address', 'foundationpress' ),
+		'section' => 'contact_social',
+		'type'    => 'text'
+	) );
+	$wp_customize->add_setting( 'sas_address' );
+	$wp_customize->add_control( 'sas_address', array(
+		'label'   => __( 'Main Address', 'foundationpress' ),
+		'section' => 'contact_social',
+		'type'    => 'textarea'
+	) );
+	$wp_customize->add_setting( 'sas_facebook' );
+	$wp_customize->add_control( 'sas_facebook', array(
+		'label'   => __( 'Facebook URL', 'foundationpress' ),
+		'section' => 'contact_social',
+		'type'    => 'text'
+	) );
+	$wp_customize->add_setting( 'sas_twitter' );
+	$wp_customize->add_control( 'sas_twitter', array(
+		'label'   => __( 'Twitter URL', 'foundationpress' ),
+		'section' => 'contact_social',
+		'type'    => 'text'
+	) );
+	$wp_customize->add_setting( 'sas_instagram' );
+	$wp_customize->add_control( 'sas_instagram', array(
+		'label'   => __( 'Instagram URL', 'foundationpress' ),
+		'section' => 'contact_social',
+		'type'    => 'text'
+	) );
+	$wp_customize->add_setting( 'sas_linkedin' );
+	$wp_customize->add_control( 'sas_linkedin', array(
+		'label'   => __( 'LinkedIn URL', 'foundationpress' ),
+		'section' => 'contact_social',
+		'type'    => 'text'
+	) );
+}
+
+add_action( 'customize_register', 'customize_register' );
