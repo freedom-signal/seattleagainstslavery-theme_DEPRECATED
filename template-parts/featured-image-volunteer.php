c4e460bf6bfdc3e6ead859e3e1d91de9cc0b805d
Marina Berti <marina@codesqwerl.com>
2018-04-01 17:19:14 -0700

    feat(volunteer): volunteer template

---
+++ template-parts/featured-image-volunteer.php
@@ -0,0 +1,14 @@
+<?php
+$background = get_stylesheet_directory_uri() . '/dist/assets/images/volunteer-bg.png';
+$class = '';
+if ( has_post_thumbnail( $post->ID ) ):
+	$background = get_the_post_thumbnail_url( $post->ID );
+	$class = 'featured-hero--thumbnail';
+endif;
+?>
+
+<div class="featured-hero featured-hero--orange <?= $class; ?>" style="background-image: url(<?= $background; ?>)">
+	<div class="featured-hero--container">
+		<?= $post->post_content ?>
+	</div>
+</div>
---
+++ template-parts/flexible-positions.php
@@ -0,0 +1,20 @@
+<?php $positions = get_field( 'positions' );
+if ( $positions['positions'] ): ?>
+	<section class="flexible flexible--positions">
+		<div class="flexible--positions--container">
+			<ul class="flexible--positions--accordion" data-accordion data-allow-all-closed="true">
+				<?php foreach ( $positions['positions'] as $position ): ?>
+					<li class="flexible--positions--single" data-accordion-item>
+						<a class="flexible--positions--single-title" href="#"><?= $position['title']; ?></a>
+						<div class="flexible--positions--single-content" data-tab-content>
+							<h3><?= __( 'Responsibilities:', 'foundationpress' ); ?></h3>
+							<?= $position['responsibilities']; ?>
+							<h3><?= __( 'Desired Skills:', 'foundationpress' ); ?></h3>
+							<?= $position['skills']; ?>
+							<a href="mailto:<?= $position['contact']; ?>?subject=<?= $position['title']; ?> volunteer position"><?= __( 'Apply Now', 'foundationpress' ); ?></a>
+						</div>
+					</li>
+				<?php endforeach; ?>
+		</div>
+	</section>
+<?php endif; ?>
