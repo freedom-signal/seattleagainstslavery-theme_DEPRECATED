88f2fe7ee0c9815677bcda00a21635096a271b3a
Marina Berti <marina@codesqwerl.com>
2018-04-03 19:07:33 -0700

    fix(templates): fix font size issues

--- template-parts/flexible-directors.php
+++ template-parts/flexible-directors.php
@@ -12,40 +12,44 @@ $the_query       = new WP_Query( array(
 
 if ( $the_query->have_posts() ): ?>
 	<section class="flexible flexible--directors">
-		<?php if ( $directors['title'] ): ?>
-			<h2 class="flexible--directors--title"><?= $directors['title']; ?></h2>
-		<?php endif; ?>
-		<div class="flexible--directors--container">
-			<?php while ( $the_query->have_posts() ):
-			$the_query->the_post(); ?>
-			<div class="flexible--directors--single">
-				<?php if ( get_field( 'email' ) ): ?>
-				<a href="mailto:<?php the_field( 'email' ); ?>">
-				<?php else: ?>
-				<div class="flexible--directors--single-container">
-				<?php endif; ?>
-					<?php
-					$thumbnail = get_stylesheet_directory_uri() . '/dist/assets/images/member.png';
-					if ( has_post_thumbnail() ):
-						$thumbnail = get_the_post_thumbnail_url();
-					endif; ?>
-					<div class="flexible--directors--single-image" style="background-image: url('<?= $thumbnail; ?>')">
-						<div class="flexible--directors--single-image--hover">
-							<?= __( 'Questions?', 'foundationpress' ); ?>
-							<?= __( 'Email Me!', 'foundationpress' ); ?>
+		<div class="main-container">
+			<div class="main-grid">
+				<div class="flexible--directors--container">
+					<?php if ( $directors['title'] ): ?>
+						<h2 class="flexible--directors--title"><?= $directors['title']; ?></h2>
+					<?php endif; ?>
+					<?php while ( $the_query->have_posts() ):
+					$the_query->the_post(); ?>
+					<div class="flexible--directors--single">
+						<?php if ( get_field( 'email' ) ): ?>
+						<a href="mailto:<?php the_field( 'email' ); ?>">
+							<?php else: ?>
+							<div class="flexible--directors--single-container">
+							<?php endif; ?>
+								<?php
+								$thumbnail = get_stylesheet_directory_uri() . '/dist/assets/images/member.png';
+								if ( has_post_thumbnail() ):
+									$thumbnail = get_the_post_thumbnail_url();
+								endif; ?>
+								<div class="flexible--directors--single-image" style="background-image: url('<?= $thumbnail; ?>')">
+									<div class="flexible--directors--single-image--hover">
+										<?= __( 'Questions?', 'foundationpress' ); ?>
+										<?= __( 'Email Me!', 'foundationpress' ); ?>
+									</div>
+								</div>
+								<div class="flexible--directors--single-content">
+									<?php the_title( '<h2>', '</h2>' ); ?>
+									<h3><?php the_field( 'title' ); ?></h3>
+								</div>
+								<?php if ( get_field( 'email' ) ): ?>
+							</a>
+							<?php else: ?>
 						</div>
+						<?php endif; ?>
 					</div>
-					<div class="flexible--directors--single-content">
-						<?php the_title( '<h2>', '</h2>' ); ?>
-						<h3><?php the_field( 'title' ); ?></h3>
-					</div>
-				<?php if ( get_field( 'email' ) ): ?>
-				</a>
-				<?php else: ?>
+					<?php endwhile; ?>
 				</div>
-				<?php endif; ?>
 			</div>
-		<?php endwhile; ?>
 		</div>
 	</section>
 <?php endif;
--- template-parts/flexible-staff.php
+++ template-parts/flexible-staff.php
@@ -12,35 +12,39 @@ $the_query   = new WP_Query( array(
 
 if ( $the_query->have_posts() ): ?>
 	<section class="flexible flexible--staff">
-		<?php if ( $staff['title'] ): ?>
-			<h2 class="flexible--staff--title"><?= $staff['title']; ?></h2>
-		<?php endif; ?>
-		<div class="flexible--staff--container">
-			<?php while ( $the_query->have_posts() ): $the_query->the_post(); ?>
-				<div class="flexible--staff--single">
-					<?php if ( get_field( 'email' ) ): ?>
-					<a href="mailto:<?php the_field( 'email' ); ?>">
+		<div class="main-container">
+			<div class="main-grid">
+				<div class="flexible--staff--container">
+					<?php if ( $staff['title'] ): ?>
+						<h2 class="flexible--staff--title"><?= $staff['title']; ?></h2>
+					<?php endif; ?>
+					<?php while ( $the_query->have_posts() ): $the_query->the_post(); ?>
+						<div class="flexible--staff--single">
+							<?php if ( get_field( 'email' ) ): ?>
+							<a href="mailto:<?php the_field( 'email' ); ?>">
+								<?php endif; ?>
+								<?php
+								$thumbnail = get_stylesheet_directory_uri() . '/dist/assets/images/member.png';
+								if ( has_post_thumbnail() ):
+									$thumbnail = get_the_post_thumbnail_url();
+								endif; ?>
+								<div class="flexible--staff--single-image" style="background-image: url('<?= $thumbnail; ?>')">
+									<div class="flexible--staff--single-image--hover">
+										<?= __( 'Questions?', 'foundationpress' ); ?>
+										<?= __( 'Email Me!', 'foundationpress' ); ?>
+									</div>
+								</div>
+								<div class="flexible--staff--single-content">
+									<?php the_title( '<h2>', '</h2>' ); ?>
+									<h3><?php the_field( 'title' ); ?></h3>
+								</div>
+								<?php if ( get_field( 'email' ) ): ?>
+							</a>
 						<?php endif; ?>
-						<?php
-						$thumbnail = get_stylesheet_directory_uri() . '/dist/assets/images/member.png';
-						if ( has_post_thumbnail() ):
-							$thumbnail = get_the_post_thumbnail_url();
-						endif; ?>
-						<div class="flexible--staff--single-image" style="background-image: url('<?= $thumbnail; ?>')">
-							<div class="flexible--staff--single-image--hover">
-								<?= __( 'Questions?', 'foundationpress' ); ?>
-								<?= __( 'Email Me!', 'foundationpress' ); ?>
-							</div>
 						</div>
-						<div class="flexible--staff--single-content">
-							<?php the_title( '<h2>', '</h2>' ); ?>
-							<h3><?php the_field( 'title' ); ?></h3>
-						</div>
-						<?php if ( get_field( 'email' ) ): ?>
-					</a>
-				<?php endif; ?>
+					<?php endwhile; ?>
 				</div>
-			<?php endwhile; ?>
+			</div>
 		</div>
 	</section>
 <?php endif;
