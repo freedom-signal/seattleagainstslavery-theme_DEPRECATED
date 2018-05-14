6aafb1bc5f36ca1fb7cbc220152ab64a5f589860
Marina Berti <marina@codesqwerl.com>
2018-05-13 21:23:27 -0700

    refactor(wp): removing unnecessary image sizes

--- library/responsive-images.php
+++ library/responsive-images.php
@@ -16,12 +16,6 @@ add_image_size( 'featured-medium', 1280, 400, true );
 add_image_size( 'featured-large', 1440, 400, true );
 add_image_size( 'featured-xlarge', 1920, 400, true );
 
-// Add additional image sizes
-add_image_size( 'fp-small', 640 );
-add_image_size( 'fp-medium', 1024 );
-add_image_size( 'fp-large', 1200 );
-add_image_size( 'fp-xlarge', 1920 );
-
 // Register the new image sizes for use in the add media modal in wp-admin
 function foundationpress_custom_sizes( $sizes ) {
 	return array_merge(
