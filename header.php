f463a02f9ab2d0d5876dc5ffe7878d070f601735
Liz Rush <liz@seattleagainstslavery.org>
2018-12-18 12:32:40 -0800

    update tracking code

--- header.php
+++ header.php
@@ -12,6 +12,16 @@
 <!doctype html>
 <html class="no-js" <?php language_attributes(); ?> >
 <head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-25825674-1"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'UA-25825674-1');
+	</script>
+
 	<meta charset="<?php bloginfo( 'charset' ); ?>"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<?php wp_head(); ?>
