f5653a13b23c52dc63e172b227b4d321f5a8fa55
Liz Rush <liz@seattleagainstslavery.org>
2019-02-14 16:07:54 -0800

    Add events to FWCAT page

--- template-parts/fwcat/fwcat-events.php
+++ template-parts/fwcat/fwcat-events.php
@@ -1,44 +1,48 @@
 <?php
 
-if(function_exists('tribe_get_events')):
-$events = tribe_get_events( array(
-	'eventDisplay'		=>	'upcoming',
-  'posts_per_page' => 3,
-  'tag' => 'FWCAT'
-) );
+if (function_exists('tribe_get_events')):
+    $events = tribe_get_events(array(
+        'eventDisplay' => 'upcoming',
+        'posts_per_page' => 3,
+        'tax_query'=> array(
+					array(
+						'taxonomy' => 'tribe_events_cat',
+						'field' => 'slug',
+						'terms' => 'FWCAT'
+					)
+				)
+    ));
 
-if ( $events ): ?>
-	<section class="flexible flexible--content events--fwcat">
-				<div class="events-container">
+    if ($events): ?>
+			  <section class="flexible flexible--content events--fwcat">
+		      <div class="events-container">
 
-					<h2 class="title">
-						<span class="spacer"></span>
-						<span><?= __( 'Upcoming FWCAT Events', 'foundationpress' ); ?></span>
-						<span class="spacer"></span>
-					</h2>
+		          <h2 class="title">
+		            <span class="spacer"></span>
+		            <span><?=__('Upcoming FWCAT Events', 'foundationpress');?></span>
+		            <span class="spacer"></span>
+		          </h2>
 
-					<div class="events-wrapper">
 
-						<?php foreach ( $events as $event ): ?>
-							<div class="single-event">
-								<a href="<?= esc_url( tribe_get_event_link( $event->ID ) ); ?>">
-                  <h4 class="date">
-                    <?= tribe_events_event_schedule_details( $event->ID ); ?>
-                  </h4>
-                  <h4><?= $event->post_title; ?></h4>
+							<?php foreach ($events as $event): ?>
+								<div class="single-event">
+									<a href="<?=esc_url(tribe_get_event_link($event->ID));?>">
+										<h5 class="date">
+											<?=tribe_events_event_schedule_details($event->ID);?>
+										</h5>
+										<h5><?=$event->post_title;?></h5>
 
-									<h4>
-										<?= tribe_get_venue( $event->ID ) ?>
-									</h4>
-								</a>
-							</div>
-						<?php endforeach; ?>
-		
-					</div>
+										<h5 class="venue">
+											<?=tribe_get_venue($event->ID)?>
+										</h5>
+									</a>
+								</div>
+							<?php endforeach;?>
+
 
           <div class="events-footer">
             <a class="button" href="/events/category/fwcat/list/">
-              <?= __( 'View More', 'foundationpress' ); ?>
+              <?=__('View more FWCAT events ⟶', 'foundationpress');?>
             </a>
           </div>
 
@@ -46,4 +50,4 @@ if ( $events ): ?>
 			</div>
 	</section>
 <?php endif;
-endif; ?>
+endif;?>
