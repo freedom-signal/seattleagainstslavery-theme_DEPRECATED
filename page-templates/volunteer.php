c2103727685d4aba403d470797eb22383320ea58
Liz Rush <liz@seattleagainstslavery.org>
2019-05-03 15:26:08 -0700

    Add FWCAT to the volunteer categories

--- page-templates/volunteer.php
+++ page-templates/volunteer.php
@@ -7,5 +7,6 @@ get_header(); ?>
 <?php get_template_part('template-parts/featured-image', 'volunteer'); ?>
 <?php get_template_part('template-parts/flexible-volunteering', 'general'); ?>
 <?php get_template_part('template-parts/flexible-volunteering', 'short-term'); ?>
+<?php get_template_part('template-parts/flexible-volunteering', 'fwcat'); ?>
 <?php get_template_part('template-parts/flexible-volunteering', 'tech'); ?>
 <?php get_footer();
