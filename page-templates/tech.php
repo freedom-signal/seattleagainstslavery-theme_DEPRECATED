159dd47d3f6cb8336886afe06557350ea7a90068
Liz Rush <liz@seattleagainstslavery.org>
2019-05-15 14:50:27 -0700

    Update tech program page

--- page-templates/tech.php
+++ page-templates/tech.php
@@ -4,17 +4,24 @@ Template Name: Tech
 */
 get_header();
 
-if ( have_rows( 'content_section' ) ):
-	while ( have_rows( 'content_section' ) ) : the_row();
-		if ( get_row_layout() == 'cta_section' ):
-			get_template_part( 'template-parts/flexible', 'cta' );
-		elseif ( get_row_layout() == 'row_section' ):
-			get_template_part( 'template-parts/flexible', 'columns' );
-		elseif (get_row_layout() == 'content_section'):
+if ( have_rows( 'intro_section' ) ):
+	while ( have_rows( 'intro_section' ) ) : the_row();
+		if ( get_row_layout() == 'content_section' ):
 			get_template_part( 'template-parts/flexible', 'content' );
 		endif;
 	endwhile;
 endif;
+
+get_template_part('template-parts/tech/tech', 'overview');
+
+if ( have_rows( 'tech_overview_section' ) ):
+	while ( have_rows( 'tech_overview_section' ) ) : the_row();
+		if ( get_row_layout() == 'content_section' ):
+			get_template_part( 'template-parts/flexible', 'content' );
+		endif;
+	endwhile;
+endif;
+
 
 get_template_part('template-parts/tech/tech', 'volunteering');
 get_footer();
---
+++ template-parts/tech/tech-overview.php
@@ -0,0 +1,71 @@
+<section class="flexible flexible--content offerings">
+	<div class="main-container">
+		<div class="main-grid">
+			<div class="offerings--container">
+				<div class="offerings--single">
+					<div class="item">
+						<span class="icon reachout"></span>
+						<h5>Victim Reachout</h5>
+						<div class="description">
+							Find potential victims online and build relationships through a
+							texting platform
+						</div>
+					</div>
+				</div>
+
+				<div class="offerings--single">
+					<div class="item">
+						<span class="icon chatbots"></span>
+						<h5>Decoy Chatbots</h5>
+						<div>
+							Decoy chatbots can pose as minor or adult sex trafficking victims
+							to interact with buyers
+						</div>
+					</div>
+				</div>
+				<div class="offerings--single">
+					<div class="item ">
+						<span class="icon deterrence"></span>
+						<h5>Deterrence Sites</h5>
+						<div>
+							Disrupt buyers in the process by redirecting them to a deterrence
+							website with alternatives
+						</div>
+					</div>
+				</div>
+
+				<div class="offerings--single">
+					<div class="item">
+						<span class="icon social"></span>
+						<h5>Social Media Reinforcement</h5>
+						<div>
+							Build profiles of buyers with the bot, then reinforce deterrence
+							messaging on social media
+						</div>
+					</div>
+				</div>
+				<div class="offerings--single">
+					<div class="item">
+						<span class="icon search"></span>
+						<h5>Search Ad Deterrence</h5>
+						<div>
+							Interrupt buyers duing the search process by using keywords to
+							target deterrence messaging
+						</div>
+					</div>
+				</div>
+
+				<div class="offerings--single">
+					<div class="item ">
+						<span class="icon analytics"></span>
+						<h5>Data Analytics</h5>
+						<div>
+							Develop insights into your data with self-service reporting and
+							responsive customer support
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
