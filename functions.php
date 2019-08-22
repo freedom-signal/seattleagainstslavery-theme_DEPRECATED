f0b3495bbef5a4612b80bac393bfc559a875433b
Liz Rush <liz@seattleagainstslavery.org>
2019-08-21 17:27:30 -0700

    Remove ugly brackets on ellipses

--- functions.php
+++ functions.php
@@ -15,64 +15,91 @@
  */
 
 /** Various clean up functions */
-require_once( 'library/cleanup.php' );
+require_once 'library/cleanup.php';
 
 /** Required for Foundation to work properly */
-require_once( 'library/foundation.php' );
+require_once 'library/foundation.php';
 
 /** Format comments */
-require_once( 'library/class-foundationpress-comments.php' );
+require_once 'library/class-foundationpress-comments.php';
 
 /** Register all navigation menus */
-require_once( 'library/navigation.php' );
+require_once 'library/navigation.php';
 
 /** Add menu walkers for top-bar and off-canvas */
-require_once( 'library/class-foundationpress-top-bar-walker.php' );
-require_once( 'library/class-foundationpress-mobile-walker.php' );
+require_once 'library/class-foundationpress-top-bar-walker.php';
+require_once 'library/class-foundationpress-mobile-walker.php';
 
 /** Create widget areas in sidebar and footer */
-require_once( 'library/widget-areas.php' );
+require_once 'library/widget-areas.php';
 
 /** Return entry meta information for posts */
-require_once( 'library/entry-meta.php' );
+require_once 'library/entry-meta.php';
 
 /** Enqueue scripts */
-require_once( 'library/enqueue-scripts.php' );
+require_once 'library/enqueue-scripts.php';
 
 /** Add theme support */
-require_once( 'library/theme-support.php' );
+require_once 'library/theme-support.php';
 
 /** Add Nav Options to Customer */
-require_once( 'library/custom-nav.php' );
+require_once 'library/custom-nav.php';
 
 /** Change WP's sticky post class */
-require_once( 'library/sticky-posts.php' );
+require_once 'library/sticky-posts.php';
 
 /** Configure responsive image sizes */
-require_once( 'library/responsive-images.php' );
+require_once 'library/responsive-images.php';
 
 /** Add theme options */
-require_once( 'library/theme-options.php' );
+require_once 'library/theme-options.php';
 
 /** Custom post types */
-require_once( 'library/custom-post-type.php' );
+require_once 'library/custom-post-type.php';
 
 /** Don't allow automatic ping backs */
-function remove_x_pingback($headers) {
-  unset($headers['X-Pingback']);
-  return $headers;
+function remove_x_pingback($headers)
+{
+	unset($headers['X-Pingback']);
+	return $headers;
 }
 add_filter('wp_headers', 'remove_x_pingback');
 
 // Remove the links to xmlrpc.php and wlwmanifest.xml
-function removeHeadLinks() {
+function removeHeadLinks()
+{
 	remove_action('wp_head', 'rsd_link');
 	remove_action('wp_head', 'wlwmanifest_link');
 }
 add_action('init', 'removeHeadLinks');
 
 // Change the description sent to stripe when a One Time Donation is made.
-add_filter( 'gform_stripe_charge_description', 'change_stripe_description', 10, 5);
-function change_stripe_description( $description, $strings, $entry, $submission_data, $feed ) {
-  return $submission_data['form_title'];
-};
+add_filter(
+	'gform_stripe_charge_description',
+	'change_stripe_description',
+	10,
+	5
+);
+function change_stripe_description(
+	$description,
+	$strings,
+	$entry,
+	$submission_data,
+	$feed
+) {
+	return $submission_data['form_title'];
+}
+
+// Make a custon excerpt for blog list
+function ce4_excerpt_length($length)
+{
+	return 60;
+}
+add_filter('excerpt_length', 'ce4_excerpt_length');
+
+function ce4_excerpt_more($more)
+{
+	global $post;
+	return '...';
+}
+add_filter('excerpt_more', 'ce4_excerpt_more');
