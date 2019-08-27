8fec8feab2e7a5333b4f0d93d0f4dbdec5920189
Liz Rush <liz@seattleagainstslavery.org>
2019-08-27 13:57:44 -0700

    update author page:

--- author.php
+++ author.php
@@ -11,25 +11,36 @@
 get_header(); ?>
 <div class="main-container">
 	<div class="main-grid">
-		<main class="main-content-full-width">
-			<?php get_template_part( 'template-parts/author', 'bio' ); ?>
-			<?php if ( have_posts() ) : ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-					<?php get_template_part( 'template-parts/content', get_post_format() === null ? get_post_format() : get_post_type() ); ?>
-				<?php endwhile; ?>
-			<?php else : ?>
-				<?php get_template_part( 'template-parts/content', 'none' ); ?>
-			<?php endif; ?>
-			<div class="post-border">
-				<hr>
-			</div>
-			<nav class="post-navigation" id="post-nav">
-				<div class="post-navigation--next"><?php previous_posts_link( __( 'View newer', 'foundationpress' ) ); ?></div>
-				<div class="post-navigation--previous"><?php next_posts_link( __( 'View older', 'foundationpress' ) ); ?></div>
+		<main class="main-content-full-width author-posts">
+      
+      <div class="author-meta">
+        <?php foundationpress_entry_meta_full(); ?>
+      </div>
+      
+      <?php if (have_posts()): ?>
+				<?php while (have_posts()):
+    	the_post(); ?>
+					<?php get_template_part(
+     	'template-parts/blog/blog',
+     	get_post_format() === null ? get_post_format() : get_post_type()
+     ); ?>
+				<?php
+    endwhile; ?>
+			<?php else: ?>
+				<?php get_template_part('template-parts/content', 'none'); ?>
+      <?php endif; ?>
+      
+      <hr>
+      
+      <nav class="post-navigation" id="post-nav">
+				<div class="post-navigation--next"><?php previous_posts_link(
+    	__('View newer', 'foundationpress')
+    ); ?></div>
+				<div class="post-navigation--previous"><?php next_posts_link(
+    	__('View older', 'foundationpress')
+    ); ?></div>
 			</nav>
-			<div class="post-border">
-				<hr>
-			</div>
+
 		</main>
 	</div>
 </div>
